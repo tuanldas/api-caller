d34ac561c9c57cab92f54474903045b93a1ed5eb
tuanldas <tuanldas@gmail.com>
2023-07-06 11:38:51 +0700

    .

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
+import axios from "axios";
+
 let defaultHeaders = {
     'Content-Type': 'application/json'
-};
+}
+
 let requestOptions = {
     method: 'get',
     headers: defaultHeaders
 };
 
-const get = (uri) => {
-    return fetch(uri, requestOptions)
-        .then(function (response) {
-            return response.json();
-        })
-        .catch(function (error) {
-        });
+const ApiCaller = {
+    get(uri) {
+        return fetch(uri, requestOptions)
+            .then(function (response) {
+                return response.json();
+            })
+            .catch(function (error) {
+            });
+    }
 }
 
-module.exports = {
-    get: get
-};
-
+export default ApiCaller
